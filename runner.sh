4a89c78be63ea286ce3dcd0e18fd4b4af87e9fd2
Aedin Lai <lai.ae@northeastern.edu>
2026-01-10 15:50:52 -0500

    new file:   runner.sh

---
+++ runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Go to the folder of this script
+cd "$(dirname "$0")"
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Start Flask in background
+python3 web_app.py &
+FLASK_PID=$!
+
+# Start Cloudflare Tunnel
+cloudflared tunnel --url http://localhost:8080
+
+# When tunnel stops, kill Flask
+kill $FLASK_PID
