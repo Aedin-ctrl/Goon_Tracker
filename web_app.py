c6d5445b61996a301959ecdf47a6aa5c6c6636c5
Aedin Lai <lai.ae@northeastern.edu>
2026-01-10 16:28:29 -0500

    new file:   how_to_run.txt 	modified:   runner.sh 	modified:   web_app.py

--- web_app.py
+++ web_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Goon Tracker!"
+    return "Test from Goon Tracker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
